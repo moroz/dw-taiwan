e023cfb959b77d703beae15dcf714966e484c9a6
Karol Moroz <k.j.moroz@gmail.com>
2019-07-17 15:03:02 +0200

    Add virtual fields and validation to guests

--- lib/diamondway/guests/guest.ex
+++ lib/diamondway/guests/guest.ex
@@ -2,6 +2,9 @@ defmodule Diamondway.Guests.Guest do
   use Ecto.Schema
   import Ecto.Changeset
 
+  @required ~w(city country email first_name last_name reference_name reference_email)a
+  @all @required ++ ~w(single_person_registration travel_insurance visa_requirements)a
+
   schema "guests" do
     field :city, :string
     field :country, :string
@@ -11,14 +14,21 @@ defmodule Diamondway.Guests.Guest do
     field :reference_email, :string
     field :reference_name, :string
 
+    field :single_person_registration, :boolean, virtual: true
+    field :travel_insurance, :boolean, virtual: true
+    field :visa_requirements, :boolean, virtual: true
+
     timestamps()
   end
 
   @doc false
   def changeset(guest, attrs) do
     guest
-    |> cast(attrs, [:first_name, :last_name, :email, :country, :city, :reference_name, :reference_email])
-    |> validate_required([:first_name, :last_name, :email, :country, :city, :reference_name, :reference_email])
+    |> cast(attrs, @all)
+    |> validate_required(@required)
     |> unique_constraint(:email)
+    |> validate_acceptance(:single_person_registration)
+    |> validate_acceptance(:travel_insurance)
+    |> validate_acceptance(:visa_requirements)
   end
 end
