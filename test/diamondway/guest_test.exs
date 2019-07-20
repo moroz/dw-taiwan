d21aded207f2a47f295a2147694d63cb22eb20c0
Karol Moroz <k.j.moroz@gmail.com>
2019-07-21 02:12:47 +0800

    Add tests for required attributes and acceptance validation

--- test/diamondway/guest_test.exs
+++ test/diamondway/guest_test.exs
@@ -5,30 +5,50 @@ defmodule Diamondway.Guests.GuestTest do
   alias Diamondway.Countries.Country
   alias Diamondway.Repo
 
-  def valid_attrs() do
+  def valid_attrs(attrs \\ []) do
     russia = Repo.get_by(Country, name: "Russia")
 
-    %{
-      city: "Moscow",
-      email: "user@example.com",
-      first_name: "Sergey",
-      last_name: "Solzhenicyn",
-      reference_name: "Dasha",
-      reference_email: "user@example.com",
-      phone: "+886912345678",
-      sex: :male,
-      nationality_id: russia.id,
-      residence_id: russia.id,
-      single_person_registration: true,
-      travel_insurance: true,
-      visa_requirements: true
-    }
+    Enum.into(
+      attrs,
+      %{
+        city: "Moscow",
+        email: "user@example.com",
+        first_name: "Sergey",
+        last_name: "Solzhenicyn",
+        reference_name: "Dasha",
+        reference_email: "user@example.com",
+        phone: "+886912345678",
+        sex: :male,
+        nationality_id: russia.id,
+        residence_id: russia.id,
+        single_person_registration: true,
+        travel_insurance: true,
+        visa_requirements: true
+      }
+    )
   end
+
+  @required ~w(city email first_name last_name reference_name reference_email phone sex nationality_id residence_id)a
+  @validates_confirmation ~w(single_person_registration travel_insurance visa_requirements)a
 
   describe "changeset/2" do
     test "is valid with valid attributes" do
       changeset = Guest.changeset(%Guest{}, valid_attrs())
       assert changeset.valid?
+    end
+
+    for attr <- @required ++ @validates_confirmation do
+      test "is invalid when #{attr} is nil" do
+        changeset = Guest.changeset(%Guest{}, valid_attrs([{unquote(attr), nil}]))
+        refute changeset.valid?
+      end
+    end
+
+    for attr <- @validates_confirmation do
+      test "is invalid when #{attr} is not accepted" do
+        changeset = Guest.changeset(%Guest{}, valid_attrs([{unquote(attr), false}]))
+        refute changeset.valid?
+      end
     end
   end
 end
