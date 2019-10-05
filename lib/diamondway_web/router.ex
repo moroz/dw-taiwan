d3e4a50b37ac24620e3eafb353106ad879fb0e70
Karol Moroz <k.j.moroz@gmail.com>
2019-10-05 19:35:36 +0800

    Show a link to dashboard when signed in

--- lib/diamondway_web/router.ex
+++ lib/diamondway_web/router.ex
@@ -7,6 +7,7 @@ defmodule DiamondwayWeb.Router do
     plug :fetch_flash
     plug :protect_from_forgery
     plug :put_secure_browser_headers
+    plug DiamondwayWeb.Plugs.FetchUser
   end
 
   pipeline :api do
