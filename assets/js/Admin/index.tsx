30a0523927fd9ef24cc6023c53b4992905f55574
Karol Moroz <k.j.moroz@gmail.com>
2019-10-11 16:05:11 +0800

    Implement layout

--- assets/js/Admin/index.tsx
+++ assets/js/Admin/index.tsx
@@ -2,6 +2,28 @@ import React from "react";
 
 export default class MahamudraAdmin extends React.Component {
   render() {
-    return <h1>Hello from React!</h1>;
+    return (
+      <div className="admin">
+        <div className="admin__left">
+          <div className="admin__header">
+            <img src="/images/logo-admin.svg"></img>
+          </div>
+          <div className="admin__menu">
+            <div className="admin__profile">
+              <div className="admin__profile__avatar">
+                <img src="/images/avatar.jpg"></img>
+              </div>
+              <p className="admin__profile__name">Umar ibn Abd al-Aziz</p>
+            </div>
+          </div>
+        </div>
+        <div className="admin__right">
+          <div className="admin__topbar">
+            <h1>Waiting List</h1>
+          </div>
+          <main className="admin__main">Hello from React!</main>
+        </div>
+      </div>
+    );
   }
 }
