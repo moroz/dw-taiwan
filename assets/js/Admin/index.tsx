b5500b7be9dffd6efad286f8c0c90b1cc5438b7b
Karol Moroz <k.j.moroz@gmail.com>
2019-10-11 17:27:25 +0800

    Implement admin navigation and design

--- assets/js/Admin/index.tsx
+++ assets/js/Admin/index.tsx
@@ -8,14 +8,38 @@ export default class MahamudraAdmin extends React.Component {
           <div className="admin__header">
             <img src="/images/logo-admin.svg"></img>
           </div>
-          <div className="admin__menu">
+          <div className="admin__sidebar">
             <div className="admin__profile">
               <div className="admin__profile__avatar">
                 <img src="/images/avatar.jpg"></img>
               </div>
               <p className="admin__profile__name">Umar ibn Abd al-Aziz</p>
             </div>
+            <div className="admin__sidebar__menu">
+              <a
+                href="javascript:void"
+                className="admin__sidebar__link admin__sidebar__link--active"
+              >
+                Waiting List
+              </a>
+              <a href="javascript:void" className="admin__sidebar__link">
+                Invited Guests
+              </a>
+              <a href="javascript:void" className="admin__sidebar__link">
+                Paid Reservations
+              </a>
+            </div>
           </div>
+          <footer className="admin__sidebar__footer">
+            <a href="/admin/logout" className="button ui primary">
+              Sign out
+            </a>
+            <p>
+              &copy; 2019 by Karol Moroz.
+              <br />
+              Licensed with BSD 3-clause license.
+            </p>
+          </footer>
         </div>
         <div className="admin__right">
           <div className="admin__topbar">
