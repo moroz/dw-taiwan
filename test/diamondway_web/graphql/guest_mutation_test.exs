882f908272a00cc05163cb8be89fbd414eb078ae
Karol Moroz <k.j.moroz@gmail.com>
2019-10-24 20:21:19 +0800

    Test audits for marking user as verified

--- test/diamondway_web/graphql/guest_mutation_test.exs
+++ test/diamondway_web/graphql/guest_mutation_test.exs
@@ -1,5 +1,6 @@
 defmodule DiamondwayWeb.GraphQL.GuestMutationTest do
   use DiamondwayWeb.GraphQLCase, schema: DiamondwayWeb.GraphQL.Schema
+  alias Diamondway.Audits
 
   setup do
     guest = insert(:guest)
@@ -15,12 +16,17 @@ defmodule DiamondwayWeb.GraphQL.GuestMutationTest do
       guest {
         id
         status
+        audits {
+          description
+          userName
+        }
       }
     }
   }
   """
   test "changes status to verified when called with correct params", %{user: user, guest: guest} do
     assert guest.status == :unverified
+    assert Audits.list_guest_audits(guest) == []
 
     %{"transition" => %{"success" => true, "guest" => res}} =
       run_with_user(@mutation, user, %{id: guest.id, to_state: :verified})
@@ -28,5 +34,8 @@ defmodule DiamondwayWeb.GraphQL.GuestMutationTest do
     assert res["status"] == "VERIFIED"
     actual = reload(guest)
     assert actual.status == :verified
+    [audit] = res["audits"]
+    assert audit["userName"] == user.display_name
+    assert audit["description"] =~ ~r/marked as verified/
   end
 end
