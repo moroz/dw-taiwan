7b219e4c79a04b8bd869f8865432f3108de85f88
Karol Moroz <k.j.moroz@gmail.com>
2019-11-22 16:08:30 +0800

    Set payment endpoint

--- config/staging.exs
+++ config/staging.exs
@@ -7,4 +7,6 @@ config :diamondway, DiamondwayWeb.Endpoint,
   http: [:inet6, port: System.get_env("PORT") || 4000],
   cache_static_manifest: "priv/static/cache_manifest.json"
 
+config :ecpay, prod: false, return_url: "https://staging.mahamudra.taipei/api/payments"
+
 config :diamondway, DiamondwayWeb.Mailer, adapter: Swoosh.Adapters.Local
