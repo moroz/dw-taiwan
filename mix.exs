73bf4d0c9fd5257f63736db662f1f129ddc9ce1f
Karol Moroz <k.j.moroz@gmail.com>
2020-01-14 16:42:33 +0800

    Update ecto

--- mix.exs
+++ mix.exs
@@ -36,8 +36,8 @@ defmodule Diamondway.MixProject do
     [
       {:phoenix, "~> 1.4.11"},
       {:phoenix_pubsub, "~> 1.1"},
-      {:phoenix_ecto, "~> 4.0"},
-      {:ecto_sql, "~> 3.0"},
+      {:phoenix_ecto, "~> 4.1"},
+      {:ecto_sql, "~> 3.3"},
       {:postgrex, ">= 0.0.0"},
       {:phoenix_html, "~> 2.11"},
       {:phoenix_live_reload, "~> 1.2", only: :dev},
