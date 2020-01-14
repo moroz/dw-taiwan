b481d0d88b2e6fddb0a85d495375365042ed88c4
Karol Moroz <k.j.moroz@gmail.com>
2020-01-14 17:25:05 +0800

    Embed map in FAQ

--- lib/diamondway_web/views/layout_view.ex
+++ lib/diamondway_web/views/layout_view.ex
@@ -1,3 +1,13 @@
 defmodule DiamondwayWeb.LayoutView do
   use DiamondwayWeb, :view
+
+  def html_tag(%Plug.Conn{} = conn) do
+    case conn.assigns[:locale] do
+      "zh" ->
+        {:safe, ~s{<html lang=\"zh-Hant\">}}
+
+      other ->
+        {:safe, ~s{<html lang=\"#{other}\">}}
+    end
+  end
 end
