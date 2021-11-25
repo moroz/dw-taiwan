9d5440f1db3f1effc8ce5777b4a9fbec51e7216f
Karol Moroz <k.j.moroz@gmail.com>
2021-11-25 15:12:21 +0100

    Get application to compile and start

--- mix.exs
+++ mix.exs
@@ -34,15 +34,15 @@ defmodule Diamondway.MixProject do
   # Type `mix help deps` for examples and options.
   defp deps do
     [
-      {:phoenix, "~> 1.4.11"},
-      {:phoenix_pubsub, "~> 1.1"},
+      {:phoenix, "~> 1.5", override: true},
+      {:phoenix_pubsub, "~> 2.0"},
       {:phoenix_ecto, "~> 4.1"},
       {:ecto_sql, "~> 3.3"},
       {:postgrex, ">= 0.0.0"},
       {:phoenix_html, "~> 2.11"},
       {:phoenix_live_reload, "~> 1.2", only: :dev},
       {:gettext, "~> 0.11"},
-      {:jason, "~> 1.0"},
+      {:jason, "~> 1.0", override: true},
       {:cowboy, "~> 2.7", override: true},
       {:plug_cowboy, "~> 2.0"},
       {:ecto_enum, "~> 1.3"},
